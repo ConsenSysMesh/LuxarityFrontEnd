912ef468054c33354e85229f818fa29835d52fa2
Robert Greenfield IV <robby.greenfield@consensys.net>
2018-10-17 19:31:38 +0300

    added basic containers of footer

--- src/layouts/components/wrapper/Header.js
+++ src/layouts/components/wrapper/Header.js
@@ -27,31 +27,31 @@ export default class Header extends React.Component {
   render() {
 
     return (
-      <div style={{paddingLeft: '10%', paddingRight: '10%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', paddingTop: 10, paddingBottom: 10, position: 'relative'}}>
+      <div style={{paddingLeft: '10%', paddingRight: '10%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', paddingTop: 15, paddingBottom: 15, position: 'relative'}}>
         <Row style={{alignItems: 'center', justifyContent: 'space-between', paddingTop: 2}}>
             <Row style={{paddingLeft: 5}}>
               <LogoRow SiteTitle={'Impactfolio'} />
               <div style={{paddingLeft: 40, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/' }} style={{textDecoration: 'none', color: 'black', float:'left'}}>HOME</Link>
+                <Link to={{pathname: '/' }} style={{textDecoration: 'none', color: '#7F8081', float:'left'}}>HOME</Link>
               </div>
               <div style={{paddingLeft: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/about' }} style={{textDecoration: 'none', color: 'black', float:'left'}}>ABOUT</Link>
+                <Link to={{pathname: '/about' }} style={{textDecoration: 'none', color: '#7F8081', float:'left'}}>ABOUT</Link>
               </div>
               <div style={{paddingLeft: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/raw' }} style={{textDecoration: 'none', color: 'black', float:'left'}}>RAW</Link>
+                <Link to={{pathname: '/raw' }} style={{textDecoration: 'none', color: '#7F8081', float:'left'}}>RAW</Link>
               </div>
               <div style={{paddingLeft: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/popup' }} style={{textDecoration: 'none', color: 'black', float:'left'}}>POPUP</Link>
+                <Link to={{pathname: '/popup' }} style={{textDecoration: 'none', color: '#7F8081', float:'left'}}>POPUP</Link>
               </div>
             </Row>
             <Row style={{paddingLeft: 5}}>
               <div style={{paddingRight: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/faq' }} style={{textDecoration: 'none', color: 'black'}}>DONATE</Link>
+                <Link to={{pathname: '/faq' }} style={{textDecoration: 'none', color: '#7F8081'}}>DONATE</Link>
               </div>
               <div style={{paddingRight: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center', borderLeft: 'thin solid grey', height: 15, width: 0.3}}>
               </div>
               <div style={{paddingRight: 20, fontSize: 12, display: 'inline-block', alignItems: 'center', justifyContent: 'center'}}>
-                <Link to={{pathname: '/about' }} style={{textDecoration: 'none', color: 'black'}}>SUPPORT</Link>
+                <Link to={{pathname: '/about' }} style={{textDecoration: 'none', color: '#7F8081'}}>SUPPORT</Link>
               </div>
             </Row>
         </Row>
