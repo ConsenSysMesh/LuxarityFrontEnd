2c543351d5dc7b9ac0a37a0f87c98d0dc7d681da
Robert Greenfield IV <robby.greenfield@consensys.net>
2018-10-18 08:57:50 +0300

    fixed up card formatting

--- src/layouts/components/projectcard/ProjectCard.js
+++ src/layouts/components/projectcard/ProjectCard.js
@@ -20,9 +20,9 @@ class ProjectCard extends Component {
     if (side.toLowerCase() === 'right') {
       return (
         <div className="p-card1">
-          <Grid container>
+          <Grid container style={{padding: 0}}>
             <Grid item xs={12} sm={2}></Grid>
-            <Grid item xs={12} sm={5}>
+            <Grid item xs={12} sm={6} style={{paddingBottom: 30}}>
               <div className="card-content">
                 <p className="card-sub-title">{this.props.cardCategory}</p>
                 <p className="card-title">{this.props.cardOrgName}</p>
@@ -32,7 +32,7 @@ class ProjectCard extends Component {
                 </p>
 
                 <div className="progress-bar">
-                  <div className="progress-bar-meter" style={{width: '26%'}}></div>
+                  <div className="progress-bar-meter" style={{width: '20%'}}></div>
                 </div>
 
                 <p className="pledge">{this.formatNum(this.props.cardPledged)}</p>
@@ -50,7 +50,7 @@ class ProjectCard extends Component {
         <div className="p-card2">
           <Grid container>
             <Grid item xs={12} sm={5}></Grid>
-            <Grid item xs={12} sm={5}>
+            <Grid item xs={12} sm={6} style={{paddingBottom: 30}}>
               <div className="card-content">
                 <p className="card-sub-title">{this.props.cardCategory}</p>
                 <p className="card-title">{this.props.cardOrgName}</p>
@@ -60,7 +60,7 @@ class ProjectCard extends Component {
                 </p>
 
                 <div className="progress-bar">
-                  <div className="progress-bar-meter" style={{width: '26%'}}></div>
+                  <div className="progress-bar-meter" style={{width: '20%'}}></div>
                 </div>
 
                 <p className="pledge">{this.formatNum(this.props.cardPledged)}</p>
