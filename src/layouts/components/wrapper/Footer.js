b3ca1478a6ac82959489c6497692a09a9eabd557
Justin Guyton <guyton.justin@gmail.com>
2018-10-19 11:44:02 -0400

    added simple input field

--- src/layouts/components/wrapper/Footer.js
+++ src/layouts/components/wrapper/Footer.js
@@ -2,6 +2,7 @@
 import React from 'react';
 //components
 import { Row } from 'react-grid-system';
+import Inputbox from '../inputbox/Inputbox'
 
 export default class Footer extends React.Component {
 
@@ -11,7 +12,10 @@ export default class Footer extends React.Component {
       <div>
         <Row style={{backgroundColor: 'white', minHeight: 60, borderTop: '1px solid #F1F2F3', alignItems: 'center', justifyContent: 'center', paddingLeft: '15%', paddingRight: '15%', margin: 0}}>
           <div style={{fontSize: 20, color: '#111112', textAlign: 'center', display: 'inline-block', paddingRight: 20}}>Stay up to date with everything luxarity</div>
-          <div style={{fontSize: 20, color: '#111112', textAlign: 'center', display: 'inline-block', paddingLeft: 20}}>BOX HERE</div>
+          <form class="inputbox" action="">
+            <input type="text" placeholder="Email" name="email"/>
+            <button type="submit"> &rarr; </button>
+          </form>
         </Row>
         <Row style={{backgroundColor: '#F1F2F3', minHeight: 300, padding: 0, margin: 0}}>
         </Row>
